83074e341e44c763419027be01adc5a2c2c3c6f0
Beatrixx_Kiddo <smsg@icloud.com>
2026-03-17 18:59:28 -0600

    Sprint 2: modulo productos con CRUD y UI en Avalonia

---
+++ src/DragonNutrex.App/Repositories/ProductoRepository.cs
@@ -0,0 +1,66 @@
+/*	Create → guarda un producto nuevo
+	Update → actualiza un producto existente
+	Delete → elimina un producto
+	GetAll → devuelve todos los productos
+	GetById → busca un producto por id*/
+
+using DragonNutrex.App.Interfaces;
+using DragonNutrex.App.Models;
+using DragonNutrex.App.Utils;
+
+namespace DragonNutrex.App.Repositories;
+
+public class ProductoRepository : IRepository<Producto>
+{
+    private readonly string _filePath = Path.GetFullPath(
+        Path.Combine(
+            AppContext.BaseDirectory,
+            "..", "..", "..", "..",
+            "DragonNutrex.App",
+            "Data",
+            "productos.json"
+        )
+    );
+
+    public void Create(Producto entity)
+    {
+        var productos = GetAll();
+        productos.Add(entity);
+        FileStorage.WriteList(_filePath, productos);
+    }
+
+    public void Update(Producto entity)
+    {
+        var productos = GetAll();
+        var index = productos.FindIndex(p => p.Id == entity.Id);
+
+        if (index == -1)
+            throw new Exception("Producto no encontrado.");
+
+        productos[index] = entity;
+        FileStorage.WriteList(_filePath, productos);
+    }
+
+    public void Delete(Guid id)
+    {
+        var productos = GetAll();
+        var producto = productos.FirstOrDefault(p => p.Id == id);
+
+        if (producto == null)
+            throw new Exception("Producto no encontrado.");
+
+        productos.Remove(producto);
+        FileStorage.WriteList(_filePath, productos);
+    }
+
+    public List<Producto> GetAll()
+    {
+        return FileStorage.ReadList<Producto>(_filePath);
+    }
+
+    public Producto? GetById(Guid id)
+    {
+        return GetAll().FirstOrDefault(p => p.Id == id);
+    }
+}
+
